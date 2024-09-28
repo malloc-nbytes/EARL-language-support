c834b960c66590d44858256c4fc042c9272a176a
malloc-nbytes <zdhdev@yahoo.com>
2024-09-27 23:52:30 -0500

    added 'as'

--- emacs/earl.el
+++ emacs/earl.el
@@ -45,7 +45,7 @@
       "when" "constructor" "this" "enum"
       "full" "almost" "float" "tuple" "list"
       "bool" "to" "continue" "option" "file"
-      "unit" "real" "any"
+      "unit" "real" "any" "as"
       ;; intrinsic functions
       "print" "println" "assert" "len" "open"
       "type" "unimplemented" "input"
