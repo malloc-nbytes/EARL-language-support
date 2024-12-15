bffc35f5251f14ea3d3b3be3402613a8f2b7f698
malloc-nbytes <zdhdev@yahoo.com>
2024-12-14 20:37:13 -0600

    updates to some keywords

--- emacs/earl.el
+++ emacs/earl.el
@@ -47,7 +47,7 @@
       "bool" "to" "continue" "option" "file"
       "unit" "real" "any" "as" "dictionary"
       "loop" "closure" "use" "exec" "case" "of"
-      "with"
+      "with" "cd"
       ;; intrinsic functions
       "print" "println" "assert" "len" "open"
       "type" "unimplemented" "input" "help"
